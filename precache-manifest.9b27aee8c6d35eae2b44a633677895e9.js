162b1d81ed976202460090b3c727d784fb5f8a1e
Stepan <stepos36@gmail.com>
2019-04-13 09:34:23 -0400

    Updates

---
+++ precache-manifest.9b27aee8c6d35eae2b44a633677895e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7dd7dca0dc22c3223f4",
+    "url": "/Stepos36/react-clicky-game/static/js/runtime~main.8f659186.js"
+  },
+  {
+    "revision": "c76696cbe16d5347aaba",
+    "url": "/Stepos36/react-clicky-game/static/js/main.676b87cc.chunk.js"
+  },
+  {
+    "revision": "69f34cc736f76ef62d94",
+    "url": "/Stepos36/react-clicky-game/static/js/2.b8126258.chunk.js"
+  },
+  {
+    "revision": "c76696cbe16d5347aaba",
+    "url": "/Stepos36/react-clicky-game/static/css/main.32e54b57.chunk.css"
+  },
+  {
+    "revision": "afb0d48c075cf899ecbb10156d936eba",
+    "url": "/Stepos36/react-clicky-game/index.html"
+  }
+];
